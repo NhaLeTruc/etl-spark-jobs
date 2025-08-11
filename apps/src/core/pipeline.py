23cc67fe8557d9f52262b55038d6d3d82135f2a0
Nha Le Truc <nha.letruc@gmail.com>
2025-08-11 08:08:21 +0700

    [CORE.PIPELINE] Initialization

---
+++ apps/src/core/pipeline.py
@@ -0,0 +1,45 @@
+# Externals
+from abc import ABC, abstractmethod
+from datetime import datetime
+from pyspark.sql import SparkSession
+
+# Internals
+from core.constants import DateTimeFormat
+from core.utils import get_or_create_spark_session
+
+class BaseDataPipeline(ABC):
+    def __init__(self, as_of_date: str):
+        """
+        Initialize the pipeline
+
+        Arg:
+            as_of_date: in format yyyy=MM-dd, e.g. '2025-01-01'
+        """
+        super().__init__()
+        self._as_of_date = datetime.strptime(as_of_date, "%Y-%m-%d")
+
+    @property
+    def as_of_date(self) -> datetime:
+        """
+        as_of_date is the date this pipeline was started
+        """
+        return self._as_of_date
+    
+    def as_of_date_fmt(self, fmt_str: str = DateTimeFormat.ISO_DATE_FMT.value) -> str:
+        """
+        as_of_date in yyyy-MM-dd format, e.g. '2025-01-01'
+        """
+        return self._as_of_date.strftime(fmt_str)
+    
+    @property
+    def spark(self) -> SparkSession:
+        """
+        SparkSession getter method
+        """
+        return get_or_create_spark_session()
+    
+    @abstractmethod
+    def run(self):
+        """
+        Entrypoint for starting pipeline.
+        """
