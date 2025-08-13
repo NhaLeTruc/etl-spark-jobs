0628da50d573d97a32287e22457c8f7e78f017bc
Nha Le Truc <nha.letruc@gmail.com>
2025-08-13 16:37:21 +0700

    [CORE.loggings] Configs for Spark loggings

---
+++ apps/src/core/conf/log_config.py
@@ -0,0 +1,39 @@
+"""
+Custom configuration for spark loggings
+"""
+# Externals
+from typing import Any, Dict, List, Protocol, Union
+from pyspark.sql import DataFrame, F
+
+
+class AnalysisCallableType(Protocol):
+    """
+    Type hint for callable in analysis_config dictionary
+    """
+    def __call__(self, df: DataFrame, **kwargs) -> Union[str, DataFrame]:
+        pass
+
+
+class LoggingConfig:
+    enable_log_and_analyze = False
+    logs: List[Dict[str, Any]] = []
+
+    analysis_config: Dict[str, Dict[str, AnalysisCallableType]] = {
+        "counts": {"operation": lambda df, **kwargs: f"Row Count: {df.count()}"},
+        "aggregate": {
+            "operation": lambda df, **kwargs: df.selectExpr(kwargs["expression"])
+        },
+        "filter_data": {
+            "operation": lambda df, **kwargs: df.filter(F.expr(kwargs["expression"]))
+        },
+        "select_data": {
+            "operation": lambda df, **kwargs: eval(
+                kwargs["expression"], {"__builtins__": None, "df":df, "F":F}
+            )
+        },
+        "add_column": {
+            "operation": lambda df, **kwargs: df.withColumnn(
+                kwargs["column_name"], F.expr(kwargs["expression"])
+            )
+        },
+    }
