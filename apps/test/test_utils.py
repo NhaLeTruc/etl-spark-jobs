44e8dcef7c6d83e44c65b992539d1852aa8dde39
Nha Le Truc <nha.letruc@gmail.com>
2025-08-18 18:09:42 +0700

    [CORE.utils] Improve cal_partition_dt

--- apps/test/test_utils.py
+++ apps/test/test_utils.py
@@ -17,6 +17,7 @@ class testCalPartitionDt(unittest.TestCase):
     def test_cal_partition_dt(self):
         logger = logging.getLogger(__name__)
         logger.info("\n\n[UNITTEST].[Utils].[function] cal_partition_dt [SUCCESS]")
+
         self.assertEqual(
             cal_partition_dt(
                 from_dt="2025-08-01",
@@ -28,6 +29,50 @@ class testCalPartitionDt(unittest.TestCase):
                 "2025-09-25"
             ]
         )
+
+        with self.assertRaises(ValueError) as cm:
+            cal_partition_dt(
+                from_dt="2025-11-01",
+                to_dt="2025-10-01",
+                num_partitions=10
+            )
+        self.assertEqual(str(cm.exception), "num_partitions too large for date bounds!")
+
+
+        with self.assertRaises(ValueError) as cm:
+            cal_partition_dt(
+                from_dt="2025-11-01",
+                to_dt="2025-11-01",
+                num_partitions=10
+            )
+        self.assertEqual(str(cm.exception), "num_partitions too large for date bounds!")
+
+
+        with self.assertRaises(ValueError) as cm:
+            cal_partition_dt(
+                from_dt="2025-08-01",
+                to_dt="2025-08-02",
+                num_partitions=10
+            )
+        self.assertEqual(str(cm.exception), "num_partitions too large for date bounds!")
+
+
+        with self.assertRaises(ValueError) as cm:
+            cal_partition_dt(
+                from_dt="2025-08-01",
+                to_dt="2025-08-03",
+                num_partitions=2
+            )
+        self.assertEqual(str(cm.exception), "Cannot find valid partition dates!")
+
+
+        with self.assertRaises(ValueError) as cm:
+            cal_partition_dt(
+                from_dt="2025-08-01",
+                to_dt="2025-08-02",
+                num_partitions=1
+            )
+        self.assertEqual(str(cm.exception), "Cannot find valid partition dates!")
 
 
 if __name__ == '__main__':
