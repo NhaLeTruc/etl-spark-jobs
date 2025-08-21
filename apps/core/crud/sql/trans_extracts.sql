729dc19c56cabe3bcc78208620bc4af40c3f8205
Nha Le Truc <nha.letruc@gmail.com>
2025-08-21 12:20:13 +0700

    [Pipeline] iterates on ingest_transactions

--- apps/core/crud/sql/trans_extracts.sql
+++ apps/core/crud/sql/trans_extracts.sql
@@ -1,5 +1,29 @@
-SELECT *
-FROM {table_name}
-WHERE {partition_column}
-BETWEEN {from_dt}
-AND {to_dt}
+SELECT
+    *
+FROM {schema_name}.rental
+LEFT JOIN {schema_name}.customer ON
+    rental.customer_id = customer.customer_id
+
+LEFT JOIN {schema_name}.`address` ON
+    customer.address_id = `address`.address_id
+
+LEFT JOIN {schema_name}.city ON
+    `address`.city_id = city.city_id
+
+LEFT JOIN {schema_name}.payment ON
+    rental.rental_id = payment.rental_id
+
+LEFT JOIN {schema_name}.inventory ON
+    rental.inventory_id = inventory.inventory_id
+
+LEFT JOIN {schema_name}.film ON
+    inventory.film_id = film.film_id
+
+LEFT JOIN {schema_name}.film_category ON
+    film.film_id = film_category.film_id
+
+LEFT JOIN {schema_name}.category ON
+    film_category.category_id = category.category_id
+
+WHERE 1=1
+AND rental.{partition_column} BETWEEN {from_dt} AND {to_dt}
--- apps/pipelines/ingest_transactions/ingest_transactions.py
+++ apps/pipelines/ingest_transactions/ingest_transactions.py
@@ -2,12 +2,13 @@
 main file of pipeline object "ingest_transactions"
 """
 # Externals
-from datetime import timedelta
+from datetime import date, timedelta
 
 # Internals
 from apps.core.constants import DateTimeFormat
 from apps.core.pipeline import BaseDataPipeline
-from apps.core.crud.minio_lake import minio_create
+from apps.core.crud.minio_lake import minio_write, minio_read
+from apps.core.conf.storage import MINIO_BUCKETS
 from apps.core.mappings.ingest_transactions_dqc import (
     bronze_dqc_json,
     silver_dqc_json,
@@ -15,16 +16,28 @@ from apps.core.mappings.ingest_transactions_dqc import (
 )
 from apps.pipelines.ingest_transactions.trans_tranforms import (
     extracts_bronze_transactions,
-    dqcheck_bronze_subsets,
     transforms_gold_transactions,
-    dqcheck_gold_subsets,
     transforms_silver_transactions,
-    dqcheck_silver_subsets,
 )
 
+# Variables
+bucket_lake = MINIO_BUCKETS["ops"]["lake"] + "/OPS/rental_bronze"
+bucket_house = MINIO_BUCKETS["ops"]["dwh"] + "/OPS/rental_silver"
+bucket_lakehouse = MINIO_BUCKETS["ops"]["lakehouse"] + "/OPS/rental_gold"
+ops_schema = "postgres"
+partition_dt = "rental_date"
+
+
+#########################################################################
+## BRONZE
 
 class BronzeIngestTransPipeline(BaseDataPipeline):
-    def __init__(self, as_of_date: str, lookback_days: int = 1):
+
+    def __init__(
+        self, 
+        as_of_date: str = date.today(), 
+        lookback_days: int = 365,
+    ):
         super().__init__(as_of_date)
         self.lookback_days = lookback_days
 
@@ -38,64 +51,82 @@ class BronzeIngestTransPipeline(BaseDataPipeline):
         df = extracts_bronze_transactions(
             from_dt=from_dt,
             to_dt=to_dt,
-            partition_column="CREATED_DATE",
-            num_partitions=10,
+            schema_name=ops_schema,
+            partition_column=partition_dt,
+            num_partitions=5,
         )
         
-        minio_create(df)
+        minio_write(
+            df=df,
+            path=bucket_lake,
+            partition_cols=partition_dt,
+        )
         
         self.enforced_dqc_checks(df,bronze_dqc_json)
 
-        dqcheck_bronze_subsets(df)
 
+#########################################################################
+## SILVER
 
 class SilverIngestTransPipeline(BaseDataPipeline):
-    def __init__(self, as_of_date: str, lookback_days: int = 1):
-        super().__init__(as_of_date)
-        self.lookback_days = lookback_days
 
+    def __init__(
+        self, 
+        as_of_date: str = date.today(),
+    ):
+        super().__init__(as_of_date)
 
     def run(self):
-        to_dt = self.as_of_date_fmt()
-        from_dt = (self.as_of_date - timedelta(days=int(self.lookback_days))).strftime(
-            DateTimeFormat.ISO_DATE_FMT.value
+        report_dt = self.as_of_date_fmt()
+
+        df = minio_read(
+            path=bucket_lake,
+            table_name="rental_bronze",
         )
 
         df = transforms_silver_transactions(
-            from_dt=from_dt,
-            to_dt=to_dt,
-            partition_column="CREATED_DATE",
-            num_partitions=10,
+            df=df,
+            report_dt=report_dt,
         )
         
-        minio_create(df)
-        
+        minio_write(
+            df=df,
+            path=bucket_lakehouse,
+            partition_cols=self.partition_column,
+        )
+                
         self.enforced_dqc_checks(df,silver_dqc_json)
 
-        dqcheck_silver_subsets(df)
 
+#########################################################################
+## GOLD
 
 class GoldIngestTransPipeline(BaseDataPipeline):
-    def __init__(self, as_of_date: str, lookback_days: int = 1):
+
+    def __init__(
+        self, 
+        as_of_date: str = date.today(),
+    ):
         super().__init__(as_of_date)
-        self.lookback_days = lookback_days
 
 
     def run(self):
-        to_dt = self.as_of_date_fmt()
-        from_dt = (self.as_of_date - timedelta(days=int(self.lookback_days))).strftime(
-            DateTimeFormat.ISO_DATE_FMT.value
+        report_dt = self.as_of_date_fmt()
+
+        df = minio_read(
+            path=bucket_lake,
+            table_name="rental_silver",
         )
 
         df = transforms_gold_transactions(
-            from_dt=from_dt,
-            to_dt=to_dt,
-            partition_column="CREATED_DATE",
-            num_partitions=10,
+            df=df,
+            report_dt=report_dt,
         )
         
-        minio_create(df)
+        minio_write(
+            df=df,
+            path=bucket_house,
+            partition_cols=partition_dt,
+        )
         
         self.enforced_dqc_checks(df,gold_dqc_json)
-
-        dqcheck_gold_subsets(df)
